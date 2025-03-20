46f6e034dfbf6c0b6855de7c23a5f73fc005ef55
wilmerrs <est.wilmerrordriguez@gmail.com>
2025-03-20 15:06:59 -0600

    👌feat: add new components and styles, update project configuration

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,24 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				ivory: {
+					100: '#FCFBF8',
+					200: '#F9F8F5',
+					500: '#C5BDA8',
+					700: '#8C6945'
+				},
+				carbon: {
+					700: '#1C1F21',
+					900: '#090A0C'
+				}
+			},
+			fontFamily: {
+				grenze: ['Grenze', 'Georgia', 'Times New Roman', 'Times', 'serif'],
+				mono: ["JetBrains Mono Variable", "JetBrains Mono", "Menlo", "Monaco", "Consolas", "Liberation Mono", "Courier New", "monospace"],
+			}
+		}
 	},
 	plugins: [],
 }
