907d0b82a4a2bbe0fc143bfcac13497d78a215d9
wilmerrs <est.wilmerrordriguez@gmail.com>
2025-03-22 09:11:43 -0600

    👌feat: add sitemap and social network links to internationalization support; update footer copyright information

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -11,33 +11,45 @@ export const ui = {
     "nav.home": "Home",
     "nav.experience": "Experience",
     "nav.blog": "Blog",
+    "nav.sitemap": "Sitemap",
     "nav.contact": "Contact",
     "nav.theme.light": "Toggle light theme",
     "nav.theme.dark": "Toggle dark theme",
     "nav.lang.en": "English",
     "nav.lang.es": "Spanish",
+    "nav.social.github": "Link to my GitHub profile",
+    "nav.social.linkedin": "Link to my LinkedIn profile",
 
     // Blog
     "blog.table.date": "Date",
     "blog.table.title": "Title",
     "blog.title": "Blog",
     "blog.post.detail.back": "Back to blog",
+
+    // Footer
+    "footer.copyright": "Wilmer Rodríguez Sánchez. All rights reserved.",
   },
   es: {
     // Navbar
     "nav.home": "Inicio",
     "nav.experience": "Experiencia",
     "nav.blog": "Blog",
+    "nav.sitemap": "Mapa del sitio",
     "nav.contact": "Contacto",
     "nav.theme.light": "Cambiar a tema claro",
     "nav.theme.dark": "Cambiar a tema oscuro",
     "nav.lang.en": "Inglés",
     "nav.lang.es": "Español",
+    "nav.social.github": "Enlace a mi perfil de GitHub",
+    "nav.social.linkedin": "Enlace a mi perfil de LinkedIn",
 
     // Blog
     "blog.table.date": "Fecha",
     "blog.table.title": "Título",
     "blog.title": "Blog",
     "blog.post.detail.back": "Volver al blog",
+
+    // Footer
+    "footer.copyright": "Wilmer Rodríguez Sánchez. Todos los derechos reservados.",
   },
 } as const;
