cf8b87deec67b9a7dc896e6e57ab3bbf84706aab
wilmerrs <est.wilmerrordriguez@gmail.com>
2025-03-24 14:19:53 -0600

    👌feat: add new icon components (Play, Pause, Upload, Shuffle, Refresh); update Button styles for improved UI consistency

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -30,8 +30,16 @@ export const ui = {
     "footer.copyright": "Wilmer Rodríguez Sánchez. All rights reserved.",
 
     // home
-    "home.presentation.experience_description": "years of experience as a Software Engineer. Design and creation of scalable and maintainable software, user-centered. Looking for the most optimal solutions to big problems, aware of the context. Lover of software engineering, data science, and intelligent systems.",
+    "home.presentation.experience_description":
+      "years of experience as a Software Engineer. Design and creation of scalable and maintainable software, user-centered. Looking for the most optimal solutions to big problems, aware of the context. Lover of software engineering, data science, and intelligent systems.",
     "home.presentation.from": "From",
+    "home.game_of_life.title": "Conway's Game of Life",
+    "home.game_of_life.generations": "Generations",
+    "home.game_of_life.tip": "Left click or drag to add cells. Right click to remove cells.",
+    "home.game_of_life.pause": "Pause",
+    "home.game_of_life.play": "Play",
+    "home.game_of_life.reset": "Reset",
+    "home.game_of_life.randomize": "Randomize",
   },
   es: {
     // Navbar
@@ -61,5 +69,12 @@ export const ui = {
     "home.presentation.experience_description":
       "años de experiencia como Ingeniero de Software. Diseño y creación de software escalable y mantenible, centrado en el usuario. Buscando las soluciones más óptimas a grandes problemas, consciente del contexto. Amante de la ingeniería de software, ciencia de datos y sistemas inteligentes.",
     "home.presentation.from": "Desde",
+    "home.game_of_life.title": "Juego de la vida de Conway",
+    "home.game_of_life.generations": "Generaciones",
+    "home.game_of_life.tip": "Clic izquierdo o arrastrar para añadir células. Clic derecho para eliminar células.",
+    "home.game_of_life.pause": "Pausar",
+    "home.game_of_life.play": "Iniciar",
+    "home.game_of_life.reset": "Reiniciar",
+    "home.game_of_life.randomize": "Random",
   },
 } as const;
